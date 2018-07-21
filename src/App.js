0ff93e60fe364018c9f46387f45ede0b37a0bc15
jkarlin929 <osida11572@gmail.com>
2018-07-20 21:27:51 -0400

    render titles on dom

--- src/App.js
+++ src/App.js
@@ -7,18 +7,6 @@ import axios from 'axios';
 const API_KEY = '5c36ad90511f463fa29cd47d247f4150';
 
 class App extends Component {
-  state = {
-    queries: []
-  }
-
-  componentDidMount() {
-    axios.get(`https://newsapi.org/v2/everything?q=bitcoin&apiKey=${API_KEY}`)
-    .then(res => {
-      console.log(res.data.articles)
-      this.setState({queries: res.data.articles})
-    })
-  }
-
   render() {
     return (
       <div>
--- src/components/articles.js
+++ src/components/articles.js
@@ -1,12 +1,32 @@
 import React from 'react';
+import axios from 'axios';
 
 const API_KEY = '5c36ad90511f463fa29cd47d247f4150';
 
 class Articles extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+    posts: []
+  }
+}
+
+  componentDidMount() {
+    axios.get(`https://newsapi.org/v2/everything?q=bitcoin&apiKey=${API_KEY}`)
+    .then(res => {
+      console.log(res.data.articles)
+      this.setState({posts: res.data.articles})
+    })
+  }
+
   render() {
     return(
       <div>
-        <p>Articles</p>
+        <ul>
+          {this.state.posts.map((post, i) =>
+            <li key={i}>{post.title}</li>
+            )}
+        </ul>
       </div>
     );
   }
